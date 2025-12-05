ac4caefafc6010f1bdb2da6a589c19024e177e95
suporte1 <suporte1@lefisc.com.br>
2025-12-05 11:10:00 -0300

    criando header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import { LiaGamepadSolid } from "react-icons/lia";
+// import logoImg from "public/logo.svg";
 
 export function Header() {
   return (
     <header className="w-full h-28 bg-slate-100 text-black px-2">
-      <div className="max-w-7xl mx-auto">
-        <nav>
+      <div className="max-w-7xl mx-auto  flex justify-center items-center  h-28 sm:justify-between">
+        <nav className="flex justify-center items-center gap-4">
           <Link href="/">
             <Image
               src="/logo.svg"
@@ -14,9 +16,17 @@ export function Header() {
               height={40}
               quality={100}
               priority={true}
+              className="w-full"
             />
           </Link>
+          <Link href="/">Games</Link>
+          <Link href="/profile">Perfil</Link>
         </nav>
+        <div className="hidden sm:flex justify-center items-center">
+          <Link href="/profile">
+            <LiaGamepadSolid size={34} color="#475569" />
+          </Link>
+        </div>
       </div>
     </header>
   );
